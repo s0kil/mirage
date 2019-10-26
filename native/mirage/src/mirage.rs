497c94f20d6f0e7f6f4f9a6a1d7c9aaaa57429d9
Sonny Scroggin <sonny@scrogg.in>
2019-10-26 12:01:31 -0500

    Use rustler_macros branch

--- native/mirage/src/mirage.rs
+++ native/mirage/src/mirage.rs
@@ -1,13 +1,12 @@
 use image::{DynamicImage, FilterType, GenericImageView, ImageFormat};
-use rustler::{Atom, Binary, Encoder, Env, NifStruct, OwnedBinary, ResourceArc, Term};
-use std::error::Error;
+use rustler::{Atom, Binary, Encoder, Env, NifResult, NifStruct, OwnedBinary, ResourceArc, Term};
 use std::io::Write as _;
 
-use crate::atoms::{error, gif, jpg, ok, png, unsupported_image_format};
+use crate::atoms::{gif, jpg, ok, png, unsupported_image_format};
 
 #[derive(NifStruct)]
 #[module = "Mirage"]
-struct Mirage {
+pub struct Mirage {
     byte_size: usize,
     extension: Atom,
     height: u32,
@@ -15,39 +14,41 @@ struct Mirage {
     resource: ResourceArc<Image>,
 }
 
-struct Image {
+pub struct Image {
     image: DynamicImage,
     format: ImageFormat,
 }
 
 /// from_bytes(path: String) -> Result<Mirage>
-pub fn from_bytes<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, rustler::Error> {
-    let bytes: Binary = args[0].decode()?;
-
-    match image::load_from_memory(bytes.as_slice()) {
+#[rustler::nif(schedule = "DirtyCpu")]
+pub fn from_bytes(binary: Binary) -> NifResult<Mirage> {
+    match image::load_from_memory(binary.as_slice()) {
         Ok(image) => {
-            if let Ok(format) = image::guess_format(&bytes.as_slice()) {
+            if let Ok(format) = image::guess_format(&binary.as_slice()) {
                 let mirage = Mirage {
-                    byte_size: bytes.len(),
+                    byte_size: binary.len(),
                     extension: extension(format),
                     width: image.width(),
                     height: image.height(),
                     resource: ResourceArc::new(Image { image, format }),
                 };
 
-                return Ok((ok(), mirage).encode(env));
+                return Ok(mirage);
             }
             return Err(rustler::Error::Atom("unsupported_image_format"));
         }
-        Err(err) => Ok((error(), err.description()).encode(env)),
+        Err(_) => Err(rustler::Error::BadArg),
     }
 }
 
 /// resize(resource: ResourceArc<Image>, width: u32, height: u32) -> Result<Vec<u8>>
-pub fn resize<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, rustler::Error> {
-    let resource: ResourceArc<Image> = args[0].decode()?;
-    let width: u32 = args[1].decode()?;
-    let height: u32 = args[2].decode()?;
+#[rustler::nif(schedule = "DirtyCpu")]
+pub fn resize(
+    env: Env,
+    resource: ResourceArc<Image>,
+    width: u32,
+    height: u32,
+) -> NifResult<(Atom, Binary, Mirage)> {
     let resized = resource
         .image
         .resize_to_fill(width, height, FilterType::Triangle);
@@ -72,9 +73,9 @@ pub fn resize<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, rustler::
                 resource,
             };
 
-            Ok((ok(), bytes, mirage).encode(env))
+            Ok((ok(), bytes, mirage))
         }
-        Err(err) => return Ok((error(), err.description()).encode(env)),
+        Err(_) => Err(rustler::Error::BadArg),
     }
 }
 
@@ -88,6 +89,6 @@ fn extension(format: ImageFormat) -> Atom {
 }
 
 pub fn load<'a>(env: Env, _info: Term<'a>) -> bool {
-    rustler::resource_struct_init!(Image, env);
+    rustler::resource!(Image, env);
     true
 }
